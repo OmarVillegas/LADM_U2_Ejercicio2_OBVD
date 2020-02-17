e33e5a77af1bfcd9244f3455e4759aac6d4c0dbb
OmarVillegas <60802802+OmarVillegas@users.noreply.github.com>
2020-02-16 21:52:48 -0700

    Add files via upload

---
+++ LADM_U1_Ejercicio2_OBVD/app/build/generated/source/buildConfig/debug/mx/edu/ittepic/ladm_u1_ejercicio2_obvd/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mx.edu.ittepic.ladm_u1_ejercicio2_obvd;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mx.edu.ittepic.ladm_u1_ejercicio2_obvd";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
